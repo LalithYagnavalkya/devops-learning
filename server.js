dfa5f19098e41d7ac750639805236f3a0395ac04
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-07-14 16:33:16 +0530

    removed

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("wtf is wrong");
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000, () => {
   console.log("this is running on port 5000");
 });
