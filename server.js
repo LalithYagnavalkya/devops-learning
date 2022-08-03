2c37f5187890d80c933c18e375874b429081b7f0
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-08-03 14:27:42 +0530

    add new route

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const port = 3001;
 app.get("/", (req, res) => {
   res.send("wtf is wrong");
 });
+app.get("/getdata", (req, res) => {
+  res.send(["hello", "how are you"]);
+});
 
 app.listen(port, () => {
   console.log(`this is running on port ${port}`);
