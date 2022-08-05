a45b0ca09225efc7a3b403c09d70b150c20b1c5c
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-08-05 10:04:43 +0530

    add cors anywhere

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.use(cors());
 // app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*", credentials: true }));
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
 const port = 3001;
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
