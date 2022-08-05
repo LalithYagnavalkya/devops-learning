c3d01351b65347dc419c1f13e2389e1560725d8d
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-08-05 11:47:53 +0530

    Add nodemon

--- server.js
+++ server.js
@@ -3,27 +3,32 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 // app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*", credentials: true }));
-var cors_proxy = require("cors-anywhere");
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
-  })
-  .listen(port, host, function () {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
-  });
-const port = 3001;
+// app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000/", credentials: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
   next();
 });
+const port = 3002;
 app.get("/", (req, res) => {
   res.send("wtf is wrong");
 });
