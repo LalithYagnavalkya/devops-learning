5d6df50f13a55e12b819a02b33eea05179bb2fd6
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-08-05 09:46:58 +0530

    Add cors

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*", credentials: true }));
 const port = 3001;
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => {
   res.send("wtf is wrong");
 });
